971ab4010062a0e96641716f51352142c70a6df4
halhenningsmoen <hal.henningsmoen@therealreal.com>
2020-06-06 16:59:37 -0600

    Pare down to minimal requirements

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,14 @@
 class PlayersController < ApplicationController
-
   # GET /players
   # GET /players.json
   def index
+    puts "player_params: #{player_params}"
     @players = Player.all
   end
   private
   # Only allow a list of trusted parameters through.
   def player_params
-    params.require(:player).permit(:name, :team, :position, :attempts_per_game, :total_attempts, :total_yards, :average_yards_per_attempt, :yards_per_game, :rushing_touchdowns, :longest_rush, :longest_rush_is_touchdown, :first_downs, :first_downs_percent, :twenty_plus, :forty_plus, :fumbles)
+    params.permit(:search, :sort_by, :ascending)
+      #Total Rushing Yards, Longest Rush and Total Rushing Touchdowns
   end
 end
--- app/views/players/_player.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! player, :id, :name, :team, :position, :attempts_per_game, :total_attempts, :total_yards, :average_yards_per_attempt, :yards_per_game, :rushing_touchdowns, :longest_rush, :longest_rush_is_touchdown, :first_downs, :first_downs_percent, :twenty_plus, :forty_plus, :fumbles, :created_at, :updated_at
-json.url player_url(player, format: :json)
