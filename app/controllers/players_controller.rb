6579bc848a682823effac356f0bd48d136406422
halhenningsmoen <hal.henningsmoen@therealreal.com>
2020-06-07 16:32:39 -0600

    All functionality working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,10 @@ class PlayersController < ApplicationController
   # GET /players.json
   def index
     @search = Player.ransack(params[:q])
-    puts "params: #{params}"
-    @players = @search.result
+    puts "#{'>'*10}params: #{params}"
+    respond_to do |format|
+      format.html { @players = @search.result.page(params[:page]) }
+      format.csv {  send_data Player.to_csv(@search.result), filename: "rushing.csv" }
+    end
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,14 @@
 class Player < ApplicationRecord
+  require 'csv'
+  self.per_page = 25
+
+  def self.to_csv(players)
+    CSV.generate do |csv|
+      data_columns = column_names.filter{ |col| !["id", "created_at", "updated_at"].include?(col) }
+      csv << data_columns
+      players.each do |player|
+        csv << player.attributes.values_at(*data_columns)
+      end
+    end
+  end
 end
